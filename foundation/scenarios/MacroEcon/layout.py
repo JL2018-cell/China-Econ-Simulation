184fdad99a9a92d377ab370e42cd62e2db9388cc
Johnson <71199326+JL2018-cell@users.noreply.github.com>
2022-02-25 22:32:11 +0800

    Create layout.py

---
+++ foundation/scenarios/MacroEcon/layout.py
@@ -0,0 +1,14 @@
+from copy import deepcopy
+
+import numpy as np
+from scipy import signal
+
+from foundation.base.base_env import BaseEnvironment, scenario_registry
+from foundation.scenarios.utils import rewards, social_metrics
+
+class agriculture_industry(BaseEnvironment):
+  def __init__(self):
+
+class energy_industry(BaseEnvironment):
+  def __init__(self):
+    
