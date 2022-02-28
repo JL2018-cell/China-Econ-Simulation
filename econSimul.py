36d527dc3fc43aa49949b8dabf46863618ce3489
Johnson <71199326+JL2018-cell@users.noreply.github.com>
2022-02-28 11:42:24 +0800

    Runnable

---
+++ econSimul.py
@@ -0,0 +1,77 @@
+from foundation.entities.resources import Resource, resource_registry
+from foundation.base.base_component import BaseComponent, component_registry
+from foundation.base.base_env import BaseEnvironment, scenario_registry
+from foundation.base.base_agent import BaseAgent, agent_registry
+import foundation
+
+#Experimenting, will be discarded finally.
+@resource_registry.add
+class Widget(Resource):
+    name = "Widget"
+    color = [1, 1, 1]
+    collectible = False # <--- Goes in agent inventory, but not in the world
+
+#Experimenting, will be discarded finally.
+@component_registry.add
+class BuyWidgetFromVirtualStore(BaseComponent):
+    name = "BuyWidgetFromVirtualStore"
+    required_entities = ["Coin", "Widget"]  # <--- We can now look up "Widget" in the resource registry
+    agent_subclasses = ["BasicMobileAgent"]
+
+    def __init__(
+        self,
+        *base_component_args,
+        widget_refresh_rate=0.1,
+        **base_component_kwargs
+    ):
+        super().__init__(*base_component_args, **base_component_kwargs)
+        self.widget_refresh_rate = widget_refresh_rate
+        self.available_widget_units = 0
+        self.widget_price = 5
+
+#Experimenting, will be discarded finally.
+def get_n_actions(self, agent_cls_name):
+    # This component adds 1 binary action that mobile agents can take: buy widget (or not).
+    if agent_cls_name == "BasicMobileAgent":
+        return 1  # Buy or not.
+
+    return None
+
+#Experimenting, will be discarded finally.
+def generate_masks(self, completions=0):
+    masks = {}
+    # Mobile agents' buy action is masked if they cannot build with their
+    # current coin or if no widgets are available.
+    for agent in self.world.agents:
+        masks[agent.idx] = np.array([
+            agent.state["inventory"]["Coin"] >= self.widget_price and self.available_widget_units > 0
+        ])
+
+    return masks
+
+@agent_registry.add
+class province(BaseAgent):
+    name = "GuangDong"
+
+#industries
+new_rsc_cls = resource_registry.get("Widget")
+print(new_rsc_cls)
+
+
+new_cmp_cls = component_registry.get("BuyWidgetFromVirtualStore")
+print(new_cmp_cls)
+
+#Province
+new_agn_cls = agent_registry.get("GuangDong")
+print(new_agn_cls)
+print(dir(new_cmp_cls))
+
+env_config = {
+    'scenario_name': 'layout_from_file/MacroEcon'
+    #to be contnued after layout construction on foundation/scenarios/MacroEcon.
+}
+
+env = foundation.make_env_instance(**env_config)
+
+
+
