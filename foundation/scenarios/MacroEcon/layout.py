d5ddd163adbae3cdcd0a804d4e07fe2bad505219
Johnson <71199326+JL2018-cell@users.noreply.github.com>
2022-02-28 11:27:08 +0800

    Update layout.py

--- foundation/scenarios/MacroEcon/layout.py
+++ foundation/scenarios/MacroEcon/layout.py
@@ -5,6 +5,7 @@ from scipy import signal
 
 from foundation.base.base_env import BaseEnvironment, scenario_registry
 from foundation.scenarios.utils import rewards, social_metrics
+from foundation.base.base_env import BaseEnvironment, scenario_registry
 
 class agriculture_industry(BaseEnvironment):
   def __init__(self):
@@ -12,3 +13,22 @@ class agriculture_industry(BaseEnvironment):
 class energy_industry(BaseEnvironment):
   def __init__(self):
     
+@scenario_registry.add
+class LayoutFromFile(BaseEnvironment):
+  """
+  Map of China.
+  """
+  name = "layout_from_file/MacroEcon"
+  
+  """
+  Industries
+  """
+  
+  agent_subclasses = ["LocalGov", "CentralGov"] #地方政府、中央政府。
+  required_industries = ["Agriculture", "Minerals"] #产业
+  
+  def __init__(self): #放进不同产业的生产能力、碳排放、GDP 贡献。
+    self.energy_cost = 100
+    
+  #参考 foundation\scenarios\simple_wood_and_stone\layout_from_file.py
+  
