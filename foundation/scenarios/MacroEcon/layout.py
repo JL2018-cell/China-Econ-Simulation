6a4f9b5e5fcaa7eeee519871cab17868c1274314
Johnson <71199326+JL2018-cell@users.noreply.github.com>
2022-03-02 10:57:45 +0800

    Update layout.py

--- foundation/scenarios/MacroEcon/layout.py
+++ foundation/scenarios/MacroEcon/layout.py
@@ -29,6 +29,10 @@ class LayoutFromFile(BaseEnvironment):
   
   def __init__(self): #放进不同产业的生产能力、碳排放、GDP 贡献。
     self.energy_cost = 100
-    
+    self.expn_per_day = 100
+    self.pt_per_day = 100
+    self.toCarbonEffcy = 0.5
+    self.toGDPEffcy = 0.5
+  
   #参考 foundation\scenarios\simple_wood_and_stone\layout_from_file.py
   
