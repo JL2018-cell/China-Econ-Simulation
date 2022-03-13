440c75f5ed5b6af15905f6ab1ce2af7c133105ba
Johnson <71199326+JL2018-cell@users.noreply.github.com>
2022-03-13 12:17:16 +0800

    Update layout.py

--- foundation/scenarios/MacroEcon/layout.py
+++ foundation/scenarios/MacroEcon/layout.py
@@ -7,18 +7,22 @@ from foundation.base.base_env import BaseEnvironment, scenario_registry
 from foundation.scenarios.utils import rewards, social_metrics
 from foundation.base.base_env import BaseEnvironment, scenario_registry
 
+@scenario_registry.add
 class agriculture_industry(BaseEnvironment):
   def __init__(self):
+    self.resource = 0
 
+@scenario_registry.add
 class energy_industry(BaseEnvironment):
   def __init__(self):
+    self.resource = 0
     
 @scenario_registry.add
-class LayoutFromFile(BaseEnvironment):
+class MacroEconLayout(BaseEnvironment):
   """
   Map of China.
   """
-  name = "layout_from_file/MacroEcon"
+  name = "layout/MacroEcon"
   
   """
   Industries
@@ -27,7 +31,7 @@ class LayoutFromFile(BaseEnvironment):
   agent_subclasses = ["LocalGov", "CentralGov"] #地方政府、中央政府。
   required_industries = ["Agriculture", "Minerals"] #产业
   
-  def __init__(self): #放进不同产业的生产能力、碳排放、GDP 贡献。
+  def __init__(self, **kwargs): #放进不同产业的生产能力、碳排放、GDP 贡献。
     self.energy_cost = 100
     self.expn_per_day = 100
     self.pt_per_day = 100
@@ -37,20 +41,43 @@ class LayoutFromFile(BaseEnvironment):
     self.agric = 100.
     self.energy = 100.
     assert self.starting_agent_coin >= 0.0
-    
-    def generate_observations(self):
-      return []
-    
-    def reset_starting_layout(self):
+    super().__init__(**kwargs)
+
+
+  def compute_reward(self):
+      return {0:0}
+  def generate_observations(self):
+      return {0:0}
+
+  def reset_agent_states(self):
       self.agric = 100.
       self.energy = 100.
+
+  def reset_starting_layout(self):
+      self.agric = 100.
+      self.energy = 100.
+
+  def scenario_step(self):
+      self.agric += 1.
+      self.energy += 1.
+
+
+  """
+  @property
+  def observe(self):
+    return []
+  
+  def reset_scenario(self):
+    self.agric = 100.
+    self.energy = 100.
+  
+  def next_step(self):
+    self.agric += 1
+    self.energy += 1
     
-    def scenario_step(self):
-      self.agric += 1
-      self.energy += 1
-      
-    def compute_reward(self):
-      return 0
-    
+  def get_rewards(self):
+    return 0
+  """
+
   #参考 foundation\scenarios\simple_wood_and_stone\layout_from_file.py
   
