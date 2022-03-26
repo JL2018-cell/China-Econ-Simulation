f8f11a2281f1f4360e41091da6a31295d3bf4401
Johnson <71199326+JL2018-cell@users.noreply.github.com>
2022-03-26 22:18:27 +0800

    Agent should be localGov, not BasicMobileAgent.
    BasicMobileAgent belongs to original program.

--- foundation/agents/mobiles.py
+++ foundation/agents/mobiles.py
@@ -6,18 +6,6 @@
 
 from foundation.base.base_agent import BaseAgent, agent_registry
 
-
-@agent_registry.add
-class BasicMobileAgent(BaseAgent):
-    """
-    A basic mobile agent represents an individual actor in the economic simulation.
-
-    "Mobile" refers to agents of this type being able to move around in the 2D world.
-    """
-
-    name = "BasicMobileAgent"
-
-
 @agent_registry.add
 class localGov(BaseAgent):
     """
@@ -26,4 +14,12 @@ class localGov(BaseAgent):
 
     name = "localGov"
 
-
+#@agent_registry.add
+#class BasicMobileAgent(BaseAgent):
+#    """
+#    A basic mobile agent represents an individual actor in the economic simulation.
+#
+#    "Mobile" refers to agents of this type being able to move around in the 2D world.
+#    """
+#
+#    name = "BasicMobileAgent"
