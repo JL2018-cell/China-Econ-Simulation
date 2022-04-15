1e235db027eae537edf9d55bf8eec28d10100546
Johnson <71199326+JL2018-cell@users.noreply.github.com>
2022-04-15 20:41:15 +0800

    localGov can take actions

--- foundation/agents/mobiles.py
+++ foundation/agents/mobiles.py
@@ -18,6 +18,7 @@ class localGov(BaseAgent):
     def __init__(self, loc, buildUpLimit, idx=None, multi_action_mode=None):
         BaseAgent.__init__(self, idx=idx, multi_action_mode=multi_action_mode)
         self.state["loc"] = loc
+        self.resource_points = 0
         self.buildUpLimit = buildUpLimit
         self.industries = ['Agriculture', 'Energy', 'Finance', 'IT', 'Minerals', 'Tourism']
         self.preference = {inv: 0.5 for inv in self.industries}
@@ -25,12 +26,38 @@ class localGov(BaseAgent):
         #(Pdb) p self.state
         #{'loc': [0, 0], 'inventory': {}, 'escrow': {}, 'endogenous': {}}
 
-#@agent_registry.add
-#class BasicMobileAgent(BaseAgent):
-#    """
-#    A basic mobile agent represents an individual actor in the economic simulation.
-#
-#    "Mobile" refers to agents of this type being able to move around in the 2D world.
-#    """
-#
-#    name = "BasicMobileAgent"
+    #See if agent usea more than buildUpLimit and resource_points than it has.
+    #type(actions): []
+    #If actions do not comply with constraint and reset flag = True, then resample actions until it complies with constraint.
+    #Otherwise, return how it violate the constraint.
+    def check_actions(self, actions, reset = False):
+        #See if agent uses all of its buildUpLimit and resource_points.
+        resourceful = True
+
+        #build up Agriculture & Energy industry < buildUpLimit
+        if actions['Construct.build_Agriculture'] > self.buildUpLimit['Agriculture']:
+            resourceful = False
+        if actions['Construct.build_Energy'] > self.buildUpLimit['Energy']:
+            resourceful = False
+
+        #build up other industries < resource point
+        builds = {k: v for k, v in actions.items() if "Construct.build_" in k}
+        builds.pop('Construct.build_Agriculture')
+        builds.pop('Construct.build_Energy')
+        #The agent use too much resource points
+        if sum([sum(v) for v in builds.values()]) > self.resource_points:
+            resourceful = False
+
+        return actions
+
+    #Update state of agent after taking actions
+    def parse_actions(self, actions):
+        print(self.idx, "parse action.")
+        for action, magnitude in actions.items():
+            if "Construct" in action:
+                if "break" in action:
+                    self.state['inventory'][action.split("_")[-1]] -= magnitude
+                elif "build" in action:
+                    self.state['inventory'][action.split("_")[-1]] += magnitude
+            elif "ContinuousDoubleAuction" in action:
+                pass
