8e314da81d072bc75a3835397067845ce7d6a9db
Johnson <71199326+JL2018-cell@users.noreply.github.com>
2022-04-28 22:06:34 +0800

    Fit into rllib

--- econSimul.py
+++ econSimul.py
@@ -1,3 +1,4 @@
+from ray.rllib.agents.ppo import PPOTrainer
 from foundation.entities.resources import Resource, resource_registry
 from foundation.base.base_component import BaseComponent, component_registry
 from foundation.base.base_env import BaseEnvironment, scenario_registry
@@ -43,132 +44,25 @@ env_config = {
     'starting_agent_resources': {"Food": 10., "Energy": 10.} #food, energy
 }
 
-env = foundation.make_env_instance(**env_config)
 
-obs = env.reset()
-
-"""
-def sample_random_action(agent, mask):
-    #Sample random UNMASKED action(s) for agent.
-    # Return a list of actions: 1 for each action subspace
-    if agent.multi_action_mode:
-        actions = {k: [1 if random.random() > 0.5 else 0 for elm in v] for k, v in mask.items()}
-        actions = agent.check_actions(actions, reset = False)
-        split_masks = np.split(mask, agent.action_spaces.cumsum()[:-1])
-        return [np.random.choice(np.arange(len(m_)), p=m_/m_.sum()) for m_ in split_masks]
-
-    # Return a single action
-    else:
-        return np.random.choice(np.arange(agent.action_spaces), p=mask/mask.sum())
-"""
+# Create an RLlib Trainer instance to learn how to act in the above
+# environment.
+trainer = PPOTrainer(
+    config={
+        # Env class to use (here: our gym.Env sub-class from above).
+        "env": MacroEconLayout,
+        # Config dict to be passed to our custom env's constructor.
+        "env_config": env_config,
+        # Parallelize environment rollouts.
+        "num_workers": 2,
+    })
 
 
-def sample_random_action(agent, mask):
-    """Sample random UNMASKED action(s) for agent."""
+# Train for n iterations and report results (mean episode rewards).
+# Since we have to guess 10 times and the optimal reward is 0.0
+# (exact match between observation and action value),
+# we can expect to reach an optimal episode reward of 0.0.
+for i in range(1):
+    results = trainer.train()
+    print(f"Iter: {i}; avg. reward={results['episode_reward_mean']}")
 
-    # Return a list of actions: 1 for each action subspace
-    if agent.multi_action_mode:
-        #localGov takes 1 ~ 3 actions in a timestep.
-        actions_num = random.randint(1, 3)
-        actions = {}
-        #Sample random actions
-        for _ in range(actions_num):
-            actions = {**actions, **agent.get_random_action()}
-        other_actions = {action: 0 for action in agent.action_dim.keys() if action not in actions.keys()}
-        return {**actions, **other_actions}
-
-    # Return a single action
-    else:
-        return agent.get_random_action()
-
-
-def sample_random_actions(env, obs):
-    """Samples random UNMASKED actions for each agent in obs."""
-        
-    actions = {
-        #a_obs['action_mask'] defined in ai_ChinaEcon\foundation\base\base_env.py:702
-        a_idx: sample_random_action(env.get_agent(a_idx), a_obs['action_mask'])
-        for a_idx, a_obs in obs.items()
-    }
-
-    #Update actions of agent
-    for a_idx, action in actions.items():
-        if a_idx != 'p':
-            env.world.agents[int(a_idx)].action.update(action)
-        else:
-            env.world.planner.action.update(action)
-
-    return (env, actions)
-
-#Alternative: Write intelligent code to choose optimal actions.
-env, actions = sample_random_actions(env, obs)
-#call step to advance the state and advance time by one tick.
-#This is 1 step only.
-obs, rew, done, info = env.step(actions)
-#Repeat until done.
-while not done["__all__"]:
-    env, actions = sample_random_actions(env, obs)
-    obs, rew, done, info = env.step(actions)
-
-print("Computation done.")
-print("obs.keys:\n", obs.keys())
-print("Investigate items.")
-for key, val in obs['0'].items(): 
-    print("{:50} {}".format(key, type(val)))
-print("Reward of agents.")
-for agent_idx, reward in rew.items(): 
-    print("{:2} {:.3f}".format(agent_idx, reward))
-print("Done")
-print(done)
-
-#Plot graph
-#Show location of agents
-xs = [x for x, y in env_config['agent_locs']]
-ys = [y for x, y in env_config['agent_locs']]
-
-#Plot Geographical location of agents
-fig, ax = plt.subplots()
-ax.scatter(xs, ys)
-for i, agent_name in enumerate(env_config['agent_names']):
-    ax.annotate(agent_name, (xs[i], ys[i]))
-#Pie chart of industry distribution for each agent: plt.pie(obs['0']['world-industries'].values(), labels = obs['0']['world-industries'].keys())
-#plt.plot(xs, ys, "ro")
-plt.show()
-
-#Plot industry distribution
-fig, axs = plt.subplots(2, env_config['n_agents'] // 2 + env_config['n_agents'] % 2)
-for i, agent_idx in enumerate(obs.keys()):
-    obs[agent_idx]['world-industries']
-    axs[i // 2][i % 2].pie(obs[agent_idx]['world-industries'].values(), labels = obs['0']['world-industries'].keys())
-plt.show()
-
-#Plot actions taken.
-fig, axs = plt.subplots(2, env_config['n_agents'] // 2)
-for i, agent_idx in enumerate(obs.keys()):
-   axs[i // 2][i % 2].hist(obs[agent_idx]['world-actions'])
-
-#Histogram
-for agent in obs.keys():
-    plt.hist(obs[agent]['world-actions'])
-    plt.show()
-    plt.hist(obs[agent]['world-industries'])
-    plt.show()
-
-
-"""
-#Not useful
-def sample_random_actions(env, obs):
-    #Samples random UNMASKED actions for each agent in obs.
-        
-    actions = {
-        a_idx: sample_random_action(env.get_agent(a_idx), a_obs['action_mask'])
-        for a_idx, a_obs in obs.items()
-    }
-
-    return actions
-
-actions = sample_random_actions(env, obs)
-
-obs, rew, done, info = env.step(actions)
-
-"""
