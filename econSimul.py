270ce7fd47f0c903bda43c8c9a42c56df59981a7
Johnson <71199326+JL2018-cell@users.noreply.github.com>
2022-05-21 21:08:49 +0800

    Fit into rllib framework.
    Let rllib knows the environment of training.

--- econSimul.py
+++ econSimul.py
@@ -1,3 +1,4 @@
+import ray
 from ray.rllib.agents.ppo import PPOTrainer
 from foundation.entities.resources import Resource, resource_registry
 from foundation.base.base_component import BaseComponent, component_registry
@@ -7,6 +8,7 @@ from foundation.scenarios.MacroEcon.layout import MacroEconLayout
 from foundation.components import component_registry
 from foundation.components.Construct import Construct
 from foundation.components.Transport import Transport
+from utils.env_wrapper import RLlibEnvWrapper
 import foundation
 import numpy as np
 import random
@@ -14,18 +16,6 @@ import matplotlib.pyplot as plt
 
 env_config = {
     'scenario_name': 'layout/MacroEcon',
-    #to be contnued after layout construction on foundation/scenarios/MacroEcon.
-    'world_size': [100, 100],
-    'n_agents': 3,
-    'agent_names': ["GuangDong", "HeBei", "XinJiang"],
-    'agent_locs': [(80, 10), (50, 50), (10, 60)],
-    'multi_action_mode_agents': True,
-    'allow_observation_scaling': False,
-    # Upper limit of industries that localGov can build per timestep.
-    'buildUpLimit': {'Agriculture': 10, 'Energy': 10},
-    'episode_length': 10, # Number of timesteps per episode
-    'flatten_observations': False,
-    'flatten_masks': False,
 
     'components': [
         #Build industries
@@ -36,27 +26,90 @@ env_config = {
         {'ContinuousDoubleAuction': {'max_num_orders': 5}},
     ],
 
+    # ===== SCENARIO CLASS ARGUMENTS =====
+    'agent_names': ["GuangDong", "HeBei", "XinJiang"],
+    'agent_locs': [(80, 10), (50, 50), (10, 60)],
+    'buildUpLimit': {'Agriculture': 10, 'Energy': 10},
     # Industries available in this world.
     'industries': ['Agriculture', 'Energy', 'Finance', \
                    'IT', 'Minerals', 'Tourism'], #Help to define actions of localGov
-
     # (optional) kwargs of the chosen scenario class
-    'starting_agent_resources': {"Food": 10., "Energy": 10.} #food, energy
+    'starting_agent_resources': {"Food": 10., "Energy": 10.}, #food, energy
+
+    # ===== STANDARD ARGUMENTS ======
+    #to be contnued after layout construction on foundation/scenarios/MacroEcon.
+    'world_size': [100, 100],
+    'n_agents': 3,
+    'episode_length': 10, # Number of timesteps per episode
+    'multi_action_mode_agents': True,
+    'multi_action_mode_planner': True,
+    'allow_observation_scaling': False,
+    # Upper limit of industries that localGov can build per timestep.
+    'flatten_observations': False,
+    'flatten_masks': False,
+    'dense_log_frequency': 1,
 }
 
+env_obj = RLlibEnvWrapper({"env_config_dict": env_config}, verbose=True)
 
-# Create an RLlib Trainer instance to learn how to act in the above
-# environment.
-trainer = PPOTrainer(
-    config={
-        # Env class to use (here: our gym.Env sub-class from above).
-        "env": MacroEconLayout,
-        # Config dict to be passed to our custom env's constructor.
-        "env_config": env_config,
-        # Parallelize environment rollouts.
+policies = {
+    "a": (
+        None,  # uses default policy
+        env_obj.observation_space,
+        env_obj.action_space,
+        {}  # define a custom agent policy configuration.
+    ),
+    "p": (
+        None,  # uses default policy
+        env_obj.observation_space_pl,
+        env_obj.action_space_pl,
+        {}  # define a custom planner policy configuration.
+    )
+}
+
+# In foundation, all the agents have integer ids and the social planner has an id of "p"
+policy_mapping_fun = lambda i: "a" if str(i).isdigit() else "p"
+policies_to_train = ["a", "p"]
+
+trainer_config = {
+    "multiagent": {
+        "policies": policies,
+        "policies_to_train": policies_to_train,
+        "policy_mapping_fn": policy_mapping_fun,
+    }
+}
+
+trainer_config.update(
+    {
         "num_workers": 2,
-    })
+        "num_envs_per_worker": 2,
+        # Other training parameters
+        "train_batch_size":  4000,
+        "sgd_minibatch_size": 4000,
+        "num_sgd_iter": 1
+    }
+)
 
+# We also add the "num_envs_per_worker" parameter for the env. wrapper to index the environments.
+env_config = {
+    "env_config_dict": env_config,
+    "num_envs_per_worker": trainer_config.get('num_envs_per_worker'),   
+}
+
+trainer_config.update(
+    {
+        "env_config": env_config        
+    }
+)
+
+# Initialize Ray
+ray.init(local_mode=True)
+
+# Create the PPO trainer.
+trainer = PPOTrainer(
+    env=RLlibEnvWrapper,
+    config=trainer_config,
+    )
 
 # Train for n iterations and report results (mean episode rewards).
 # Since we have to guess 10 times and the optimal reward is 0.0
