c1f82ab1d34632d341898ed0fa9b98c1b58e99c6
Johnson <71199326+JL2018-cell@users.noreply.github.com>
2022-06-26 20:48:25 +0800

    Incorporate data

--- numpy_approach/main.py
+++ numpy_approach/main.py
@@ -1,9 +1,12 @@
+from obtain_data import obtain_data, industry_dstr_over_time
+from obtain_data import industry_dstr_over_time as all_industry_dstr_over_time
 import numpy as np
-import math
-import pandas as pd
 import matplotlib.pyplot as plt
 from mpl_toolkits.axes_grid1 import make_axes_locatable
 from itertools import product               # Cartesian product for iterators
+import math
+import random
+import pdb
 
 # allow us to re-use the framework from the src directory
 import sys, os
@@ -15,76 +18,59 @@ import optimizer as O                       # stochastic gradient descent optimi
 import solver as S                          # MDP solver (value-iteration)
 import plot as P                            # helper-functions for plotting
 
-PROVINCES = ["GuangDong", "HeBei", "XinJiang"]
+DATA_PATH = './data'
+industry_init_dstr, contribution = obtain_data(DATA_PATH)
+INDUSTRIES_CHIN = ["农林牧渔业", "电力、热力、燃气及水生产和供应业", "金融业", "信息传输、软件和信息技术服务业",  "采矿业", "住宿和餐饮业", "制造业", "建筑业", "交通运输、仓储和邮政业", "批发和零售业", "教育业"]
+INDUSTRIES = ['Agriculture', 'Energy', 'Finance', 'IT', 'Minerals', 'Tourism', 'Manufacturing', 'Construction', 'Transport', 'Retail', 'Education']
+province = list(industry_init_dstr.keys())[0]
+buildUpLimit = {'Agriculture': 10, 'Energy': 10}
+# Industries available in this world. Their upper limit.
+industries_limit = {industry: 2000 for industry in INDUSTRIES}
 
-def setup_mdp(max_scale):
+def setup_mdp(industry_dstr, buildUpLimit, industries_limit, resourcePt_contribution):
     # create our world
-    world = W.GridWorld(max_scale)
+    world = W.IcyGridWorld(industry_dstr, buildUpLimit, industries_limit, resourcePt_contribution, dim = len(INDUSTRIES), size = 2, p_slip = 0.)
 
     # set up the reward function
-    CO2 = {'Agriculture': 2000, 'Energy': 2000, 'Finance': 2000, \
-           'IT': 2000, 'Minerals': 2000, 'Tourism': 2000}
-    GDP = {'Agriculture': 2000, 'Energy': 2000, 'Finance': 2000, \
-           'IT': 2000, 'Minerals': 2000, 'Tourism': 2000}
-    reward = lambda s: np.dot(np.array(GDP.values), s) - np.dot(np.array(CO2.values), s)
+    reward = np.zeros(world.n_states)
+    reward[-1] = 1.0
+    reward[8] = 0.65
 
-    # No terminal state
+    # set up terminal states
     terminal = []
 
     return world, reward, terminal
 
+# set-up the GridWorld Markov Decision Process
+# world, reward, terminal = setup_mdp(industry_init_dstr[province], buildUpLimit, industries_limit, contribution["resource_points"][province])
 
-# generate some "expert" trajectories from data.
-def generate_expert_trajectories(reduce_scale):
-    gdp = pd.read_excel('Data.xlsx', sheet_name = 0, header = 0, index_col = 0)
-    gdp = gdp.fillna(0)
-    #gdp = gdp.apply(lambda x: x/reduce_scale)
-    #gdp = gdp.round(decimals = 0)
-    #gdp = gdp.astype('int32')
-    pollutant = pd.read_excel('Data.xlsx', sheet_name = 1, header = 0, index_col = 0)
-    pollutant = pollutant.fillna(0)
-    #pollutant = pollutant.apply(lambda x: x/reduce_scale)
-    #pollutant = pollutant.round(decimals = 0)
-    #pollutant = pollutant.astype('int32')
-    industry = pd.read_excel('Data.xlsx', sheet_name = 2, header = 0, index_col = 0)
-    industry = industry.fillna(0)
-    #industry = industry.apply(lambda x: x/reduce_scale)
-    #industry = industry.round(decimals = 0)
-    #industry = industry.astype('int32')
+# Reduce number of industry, For testing only.
+#industry_init_dstr[province] = dict([(k, v) for i, (k, v) in enumerate(industry_init_dstr[province].items()) if i < 4])
+#industries_limit = dict([(k, v) for i, (k, v) in enumerate(industries_limit.items()) if i < 4])
+#contribution["resource_points"][province] = dict([(k, v) for i, (k, v) in enumerate(contribution["resource_points"][province].items()) if i < 4])
 
-    tjs = {}
-    max = 0
+world, reward, terminal = setup_mdp(industry_init_dstr[province], buildUpLimit, industries_limit, contribution["resource_points"][province])
 
-    # format:
-    # {province name: array([[state 1], [state 2], [state 3], ... [final state]])}
-    # 1 trajectory for each province
-    for prvn in PROVINCES:
-        tjs[prvn] = []
-        hist = industry.loc[[idx for idx in industry.index if prvn in idx]].T.to_numpy()
-        hist = hist // reduce_scale + (hist % reduce_scale > (reduce_scale/2))
-        hist = hist.astype('int32')
-        print("Max:", hist.max())
-        max = hist.max() if hist.max() > max else max
-        for i in range(hist.shape[0] - 1):
-           # [(state, action, next_state)]
-           tjs[prvn].append((hist[i], hist[i + 1] - hist[i], hist[i + 1]))
-        tjs[prvn] = [T.Trajectory(tjs[prvn])]
+all_industry_dstr_over_time = industry_dstr_over_time(DATA_PATH)
+industry_dstr_over_time = all_industry_dstr_over_time[province]
 
-    policy = []
-    return tjs, max
+def align(arr, target_arr):
+    assert len(arr) == len(target_arr)
+    indices = []
+    for elm in target_arr:
+        for el in arr:
+            if elm in el:
+                indices.append(el)
+    return indices
 
+indices = [[col for col in industry_dstr_over_time.columns if industry in col][0] for industry in INDUSTRIES_CHIN]
+indices = align(indices, INDUSTRIES_CHIN)
+industry_dstr_over_time = industry_dstr_over_time[indices]
+trajectories = [industry_dstr_over_time.to_numpy()]
 
-# Given {"a": 2, "b": 3} and both upper limit = 5, then return [1, 1, 0, 0, 0] + [1, 1, 1, 0, 0]
-def state_to_state_vector(industries, state):
-    state_vector = np.array([np.concatenate((np.ones(state[i]), np.zeros(v - state[i])), axis = 0) \
-                                for i, v in enumerate(industries.values())]).flatten()
-    return state_vector
-
-# normalize values in dictionary
-def normalize_dict(d):
-    vs = np.array([v for v in d.values()])
-    return dict(zip(d.keys(), vs / vs.sum()))
-
+# Convert data to binary grids.
+max_vals = [[int(math.ceil(t.max())) // 2 + 1 for t in trajectory.T] for trajectory in trajectories]
+trajectories = [(trajectory // np.array(max_vals[i])).astype('int32') for i, trajectory in enumerate(trajectories)]
 
 # == The Actual Algorithm ==
 
@@ -94,8 +80,9 @@ def feature_expectation_from_trajectories(features, trajectories):
     fe = np.zeros(n_features)
 
     for t in trajectories:                  # for each trajectory
-        for s in t.states():                # for each state in trajectory
-            fe += features[s, :]            # sum-up features
+        for s in t:                # for each state in trajectory
+            _s = world.state_point_to_index(s)
+            fe += features[_s, :]            # sum-up features
 
     return fe / len(trajectories)           # average over trajectories
 
@@ -103,10 +90,19 @@ def initial_probabilities_from_trajectories(n_states, trajectories):
     p = np.zeros(n_states)
 
     for t in trajectories:                  # for each trajectory
-        p[t.transitions()[0][0]] += 1.0     # increment starting state
+        _t = world.state_point_to_index(t[0])
+        p[_t] += 1.0     # increment starting state
 
     return p / len(trajectories)            # normalize
 
+def sampling(num_range):
+    return [random.randrange(0, num_range + 1) for _ in range(random.randrange(1, num_range))]
+
+def safe_mult(a, b):
+    if a == 0. and b == float('inf'):
+        return 0.
+    else:
+        return a * b
 
 def compute_expected_svf(p_transition, p_initial, terminal, reward, eps=1e-5):
     n_states, _, n_actions = p_transition.shape
@@ -117,23 +113,35 @@ def compute_expected_svf(p_transition, p_initial, terminal, reward, eps=1e-5):
     zs = np.zeros(n_states)                             # zs: state partition function
     zs[terminal] = 1.0
 
+    k = 23
     # 2. perform backward pass
-    for _ in range(2 * n_states):                       # longest trajectory: n_states
+    # longest trajectory: n_states
+    for _ in random.sample(range(2 * n_states), k = k):
         # reset action values to zero
         za = np.zeros((n_states, n_actions))            # za: action partition function
 
         # for each state-action pair
-        for s_from, a in product(range(n_states), range(n_actions)):
+        for s_from, a in product(random.sample(range(n_states), k = k), random.sample(range(n_actions), k = k)):
 
             # sum over s_to
-            for s_to in range(n_states):
-                za[s_from, a] += p_transition[s_from, s_to, a] * np.exp(reward[s_from]) * zs[s_to]
+            for s_to in random.sample(range(n_states), k = k):
+                za[s_from, a] += safe_mult(p_transition.p_trans(s_from, s_to, a), np.exp(reward[s_from])) * zs[s_to]
+                if np.any(np.isnan(za)):
+                    print("Alert! za nan.")
+                    pdb.set_trace()
+                if np.any(np.isnan(zs)):
+                    print("Alert! zs nan.")
+                    pdb.set_trace()
         
         # sum over all actions
         zs = za.sum(axis=1)
 
     # 3. compute local action probabilities
     p_action = za / zs[:, None]
+    np.nan_to_num(p_action, 0, 0)
+    if np.any(np.isnan(p_action)):
+        print("Alert! p_action nan.")
+        pdb.set_trace()
 
     # Forward Pass
     # 4. initialize with starting probability
@@ -141,14 +149,18 @@ def compute_expected_svf(p_transition, p_initial, terminal, reward, eps=1e-5):
     d[:, 0] = p_initial
 
     # 5. iterate for N steps
-    for t in range(1, 2 * n_states):                    # longest trajectory: n_states
+    for t in random.sample(range(1, 2 * n_states), k = k):                    # longest trajectory: n_states
         
         # for all states
-        for s_to in range(n_states):
+        for s_to in random.sample(range(n_states), k = k):
             
             # sum over nonterminal state-action pairs
-            for s_from, a in product(nonterminal, range(n_actions)):
-                d[s_to, t] += d[s_from, t-1] * p_action[s_from, a] * p_transition[s_from, s_to, a]
+            for s_from, a in product(random.sample(nonterminal, k = k), random.sample(range(n_actions), k = k)):
+                d[s_to, t] += d[s_from, t-1] * p_action[s_from, a] * p_transition.p_trans(s_from, s_to, a)
+
+    if np.any(np.isnan(d)):
+        print("Alert! d nan.")
+        pdb.set_trace()
 
     # 6. sum-up frequencies
     return d.sum(axis=1)
@@ -169,6 +181,8 @@ def maxent_irl(p_transition, features, terminal, trajectories, optim, init, eps=
 
     optim.reset(omega)              # re-start optimizer
     while delta > eps:              # iterate until convergence
+        print(delta)
+
         omega_old = omega.copy()
 
         # compute per-state reward from features
@@ -179,27 +193,13 @@ def maxent_irl(p_transition, features, terminal, trajectories, optim, init, eps=
         grad = e_features - features.T.dot(e_svf)
 
         # perform optimization step and compute delta for convergence
-        optim.step(grad)
+        optim.step(-grad)
         
         # re-compute detla for convergence check
         delta = np.max(np.abs(omega_old - omega))
 
     # re-compute per-state reward and return
     return features.dot(omega)
-
-# == The Main Program ==
-
-reduce_scale = 30 # 1/10 of original data magnitude.
-
-print("Read trajectories.")
-
-# generate some "expert" trajectories (and its policy for visualization)
-trajectories, max_scale = generate_expert_trajectories(reduce_scale)
-
-print("Set up gridworld.")
-
-# set-up the GridWorld Markov Decision Process
-world, reward, terminal = setup_mdp(max_scale)
 
 print("Set up features.")
 
@@ -224,52 +224,5 @@ print("RL learning...")
 reward_maxent = maxent_irl(world.p_transition, features, terminal, trajectories, optim, init)
 
 print("Done!")
-
-
-"""
-fig = plt.figure()
-ax = fig.add_subplot(121)
-ax.title.set_text('Original Reward')
-divider = make_axes_locatable(ax)
-cax = divider.append_axes('right', size='5%', pad=0.05)
-p = P.plot_state_values(ax, world, reward, **style)
-P.plot_deterministic_policy(ax, world, S.optimal_policy(world, reward, 0.8), color='red')
-fig.colorbar(p, cax=cax)
-
-ax = fig.add_subplot(122)
-ax.title.set_text('Recovered Reward')
-divider = make_axes_locatable(ax)
-cax = divider.append_axes('right', size='5%', pad=0.05)
-p = P.plot_state_values(ax, world, reward_maxent, **style)
-P.plot_deterministic_policy(ax, world, S.optimal_policy(world, reward_maxent, 0.8), color='red')
-fig.colorbar(p, cax=cax)
-
-fig.tight_layout()
-#plt.show()
-plt.savefig('plot2.png', dpi=250)
-
-# Note: this code will only work with one feature per state
-p_initial = initial_probabilities_from_trajectories(world.n_states, trajectories)
-e_svf = compute_expected_svf(world.p_transition, p_initial, terminal, reward_maxent)
-e_features = feature_expectation_from_trajectories(features, trajectories)
-
-fig = plt.figure()
-ax = fig.add_subplot(121)
-ax.title.set_text('Trajectory Feature Expectation')
-divider = make_axes_locatable(ax)
-cax = divider.append_axes('right', size='5%', pad=0.05)
-p = P.plot_state_values(ax, world, e_features, **style)
-fig.colorbar(p, cax=cax)
-
-ax = fig.add_subplot(122)
-ax.title.set_text('MaxEnt Feature Expectation')
-divider = make_axes_locatable(ax)
-cax = divider.append_axes('right', size='5%', pad=0.05)
-p = P.plot_state_values(ax, world, features.T.dot(e_svf), **style)
-fig.colorbar(p, cax=cax)
-
-fig.tight_layout()
-#plt.show()
-plt.savefig('plot3.png', dpi=250)
-
-"""
+print(reward_maxent)
+np.savetxt("foo.csv", reward_maxent, delimiter=",")
