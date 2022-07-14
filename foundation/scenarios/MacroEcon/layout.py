13f5fcf6ca6862b8fb1d4dcae793489302997304
Johnson <71199326+JL2018-cell@users.noreply.github.com>
2022-07-14 11:04:32 +0800

    Apply log regression

--- foundation/scenarios/MacroEcon/layout.py
+++ foundation/scenarios/MacroEcon/layout.py
@@ -40,15 +40,16 @@ class MacroEconLayout(BaseEnvironment):
       self.toGDPEffcy = 0.5
       self.contribution_chg_rate = contribution_chg_rate
       self.contribution = contribution
+      self.buildUpLimit = kwargs['buildUpLimit']
       #self.resourcePt_contrib = contribution["resource_points"]
       #self.GDP_contrib = contribution["GDP"]
       #self.CO2_contrib = contribution["CO2"]
       self.agric = starting_agent_resources["Food"]
       self.energy = starting_agent_resources["Energy"]
       self.resource_points = 100.
-      #assert self.starting_agent_coin >= 0.0
       kwargs = {**kwargs, **{"industries": self.required_entities}}
       super().__init__(**kwargs)
+      self.backup = {"contribution_chg_rate": contribution_chg_rate, "contribution": contribution, "buildUpLimit": kwargs['buildUpLimit'], "industry_init_dstr": kwargs["industry_init_dstr"]}
       # Use inverse reinforcement learning to define reward function of agents.
       if self.irl:
           root, directory, files = list(os.walk("./irl_maxent"))[0]
@@ -60,6 +61,7 @@ class MacroEconLayout(BaseEnvironment):
               self.rewards = irl_maxent.main.irl_maxent(self.irl_data_path, list(industry_depreciation.keys()), list(industries.keys()), industries_chin, kwargs["industry_init_dstr"], contribution, kwargs["buildUpLimit"], industries)
               with open('./irl_maxent/rewards.pkl', 'wb') as f:
                   pickle.dump(self.rewards, f)
+      # Upper limit of industry that each agent can build.
       for industry, upperLimits in industries.items():
           if isinstance(upperLimits, list): #Each region has its own upper limit of building industries.
               for i, agent in enumerate(self.world.agents):
@@ -99,7 +101,7 @@ class MacroEconLayout(BaseEnvironment):
               rewards[agent.idx] = 0
               max_val = max(agent.state['inventory'].values())
               tmp = max_val
-              enum = 0
+              enum = 0 # Count iteration
               while(tmp > 1):
                   idx = self.state_point_to_index(np.array(list(agent.state['inventory'].values())) > tmp)
                   rewards[agent.idx] += self.rewards[agent.state['name']][idx, enum]
@@ -113,7 +115,8 @@ class MacroEconLayout(BaseEnvironment):
       else:
           rewards = {}
           for agent in self.world.agents:
-              rewards[agent.idx] = sum(agent.industry_weights.values()) * agent.state['endogenous']['GDP'] - agent.state['endogenous']['CO2']
+              #rewards[agent.idx] = sum(agent.industry_weights.values()) * agent.state['endogenous']['GDP'] - agent.state['endogenous']['CO2']
+              rewards[agent.idx] = agent.state['endogenous']['GDP'] - agent.state['endogenous']['CO2']
           rewards[self.world.planner.idx] = self.total_GDP - self.total_CO2
           #print("In layout, rewards:", rewards)
           return rewards
@@ -136,12 +139,40 @@ class MacroEconLayout(BaseEnvironment):
       return obs
 
   def reset_agent_states(self):
+      for agent in self.world.agents:
+          self.world.agents[agent.idx].buildUpLimit = copy.copy(self.backup["buildUpLimit"])
+          for k in self.world.agents[agent.idx].endogenous.keys():
+              self.world.agents[agent.idx].endogenous[k] = 0
+          for k in self.world.agents[agent.idx].escrow.keys():
+              self.world.agents[agent.idx].escrow[k] = 0
+          for k in self.world.agents[agent.idx].industry_weights.keys():
+              self.world.agents[agent.idx].industry_weights[k] = 1
+          for k in self.world.agents[agent.idx].inventory.keys():
+              self.world.agents[agent.idx].inventory[k] = self.backup["industry_init_dstr"][agent.state["name"]][k]
+          for k in self.world.agents[agent.idx].state.keys():
+              if k == 'escrow':
+                  for ky in self.world.agents[agent.idx].state[k].keys():
+                      self.world.agents[agent.idx].state[k][ky] = 0
+              if k == 'endogenous':
+                  for ky in self.world.agents[agent.idx].state[k].keys():
+                      self.world.agents[agent.idx].state[k][ky] = 0
+              if k == 'buildUpLimit':
+                  for ky in self.world.agents[agent.idx].state[k].keys():
+                      self.world.agents[agent.idx].state[k][ky] = copy.copy(self.backup["buildUpLimit"][ky])
+              if k == 'resource_points':
+                  self.world.agents[agent.idx].state[k] = 100
+          self.world.agents[agent.idx].state["inventory"] = copy.copy(self.backup["industry_init_dstr"][agent.state["name"]])
       self.agric = 100.
       self.energy = 100.
 
   def reset_starting_layout(self):
       self.agric = 100.
       self.energy = 100.
+      self.total_CO2 = 0.
+      self.total_GDP = 0.
+      self.contribution_chg_rate = copy.copy(self.backup["contribution_chg_rate"])
+      self.contribution = copy.copy(self.backup["contribution"])
+      self.buildUpLimit = copy.copy(self.backup["buildUpLimit"])
 
   def linear_exp(self, x):
       if x > 0:
@@ -178,8 +209,8 @@ class MacroEconLayout(BaseEnvironment):
               else:
                   self.world.agents[idx].state['inventory'][industry] = 0
 
-      self.total_CO2 = sum([agent.state['endogenous']['CO2'] for agent in self.world.agents])
-      self.total_GDP = sum([agent.state['endogenous']['GDP'] for agent in self.world.agents])
+      self.total_CO2 += sum([agent.state['endogenous']['CO2'] for agent in self.world.agents])
+      self.total_GDP += sum([agent.state['endogenous']['GDP'] for agent in self.world.agents])
       self.agric += 1.
       self.energy += 1.
 
