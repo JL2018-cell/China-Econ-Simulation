575046d269112f89047a9bf64af063aa03ba55b9
Johnson <71199326+JL2018-cell@users.noreply.github.com>
2022-07-26 21:33:09 +0800

    Add files via upload

---
+++ data/result.js
@@ -0,0 +1,23 @@
+diffDATA = {};
+jsonDATA = {};
+
+diffDATA[0] = '{}';
+diffDATA[1] = '{}';
+diffDATA[2] = '{}';
+diffDATA[3] = '{}';
+diffDATA[4] = '{}';
+diffDATA[5] = '{}';
+diffDATA[6] = '{}';
+diffDATA[7] = '{}';
+diffDATA[8] = '{}';
+diffDATA[9] = '{}';
+diffDATA[10] = '{}';
+diffDATA[11] = '{}';
+diffDATA[12] = '{}';
+diffDATA[13] = '{}';
+diffDATA[14] = '{}';
+diffDATA[15] = '{}';
+diffDATA[16] = '{}';
+
+
+
